b7af794fea6ee1a2849a56443448a4cc73843827
L. Yasril Imam <60500658+yasrilimam98@users.noreply.github.com>
2022-08-20 06:13:47 +0700

    firs init

---
+++ database/factories/CategoriesFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Categories;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoriesFactory extends Factory
+{
+    protected $model = Categories::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
